28264fe0fb9e0a323da460b6b721b666092a288a
Daniel Valentini <duvalentini@gmail.com>
2016-06-03 11:51:32 -0400

    Added credential validation to middleware.

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,48 @@
 <?php
-// Application middleware
-//fffff
-// e.g: $app->add(new \Slim\Csrf\Guard);
+// Route middleware
+$mw = function ($request, $response, $next) {
+  $data = $request->getParsedBody();
+  $email = filter_var($data['email'], FILTER_SANITIZE_STRING);
+  $password = filter_var($data['password'], FILTER_SANITIZE_STRING);
+
+  if (!empty($email) && !empty($password)) {
+    //Validate credentials
+    $json_data = json_decode(file_get_contents('../docs/users.json'), true);
+    if (validateUser($email, $password, $json_data)) {
+      $newResponse = $response->withAddedHeader('Response_code', 'good');
+      if (isAdmin($email, $json_data)) {
+        $newResponse = $newResponse->withAddedHeader('Admin', 'yes');
+      }
+    } else {
+      $newResponse = $response->withAddedHeader('Response_code', 'bad_creds');
+    }
+  } elseif (!empty($password)) {
+      $newResponse = $response->withAddedHeader('Response_code', 'e_empty');
+  } elseif (!empty($email)) {
+      $newResponse = $response->withAddedHeader('Response_code', 'p_empty');
+  } else {
+      $newResponse = $response->withAddedHeader('Response_code', 'all_empty');
+  }
+  $response = $next($request, $newResponse);
+  return $response;
+};
+
+//Helper function to validate credentials
+function validateUser($email, $password, $json_data) {
+    foreach ($json_data as $user) {
+      if ($user['email'] == $email && $user['password'] == $password) {
+        return true;
+      }
+    }
+    return false;
+}
+
+//Helper function to determine if user is admin
+function isAdmin($email, $json_data) {
+    foreach ($json_data as $user) {
+      if ($user['email'] == $email && $user['admin'] == true) {
+        return true;
+      }
+    }
+    return false;
+}
--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
-//Add middleware closure
+//Add middleware closure to authenticate users
 //$app->add(function ($request, $response, ))
 
 // Routes
@@ -15,49 +15,41 @@ $app->post('/signin', function ($request, $response, $args) {
     //Get form data
     $data = $request->getParsedBody();
     $email = filter_var($data['email'], FILTER_SANITIZE_STRING);
-    $password = filter_var($data['password'], FILTER_SANITIZE_STRING);
-    if (!empty($email) && !empty($password)) {
 
-      //Validate credentials
-      $json_data = json_decode(file_get_contents('../docs/users.json'), true);
-      if (validateUser($email, $password, $json_data)) {
+    //Switch case to determine template to render
+    switch ($response->getHeader('Response_code')[0]) {
+      case 'all_empty':
+        return $this->view->render($response, 'index.html', [
+          'blankforms' => 'Please enter your email address and password.'
+        ]);
+        break;
+      case 'good':
+        $specialStuff = '';
+        if ($response->hasHeader('Admin')) {
+          $specialStuff = 'You have admin access.';
+        }
         return $this->view->render($response, 'newpage.twig', [
           'email' => $email,
-          'router' => $this->router
+          'admin' => $specialStuff
         ]);
-      } else {
-          return $this->view->render($response, 'index.html', [
-            'blankforms' => 'Invalid credentials.'
-          ]);
-      }
-    } elseif (empty($email) && !empty($password)) {
-      $blankform = 'Please enter your email address.';
-
-      return $this->view->render($response, 'index.html', [
-      'blankforms' => $blankform
-      ]);
-    } elseif (empty($password) && !empty($email)) {
-      $blankform = 'Please enter your password.';
-
-      return $this->view->render($response, 'index.html', [
-      'blankforms' => $blankform
-      ]);
-    } else {
-      $blankform = 'Please enter your email address and password.';
-
-      return $this->view->render($response, 'index.html', [
-      'blankforms' => $blankform
-      ]);
+        break;
+      case 'bad_creds':
+        return $this->view->render($response, 'index.html', [
+          'blankforms' => 'Invalid credentials.'
+        ]);
+        break;
+      case 'e_empty':
+        return $this->view->render($response, 'index.html', [
+          'blankforms' => 'Please enter your email address.'
+        ]);
+        break;
+      case 'p_empty':
+        return $this->view->render($response, 'index.html', [
+          'blankforms' => 'Please enter your password.'
+        ]);
+        break;
+      default:
+        echo 'Error';
     }
 
-})->setName("welcome-page");
-
-//Function to validate credentials
-function validateUser($email, $password, $json_data) {
-    foreach ($json_data as $user) {
-      if ($user['email'] == $email && $user['password'] == $password) {
-        return true;
-      }
-    }
-    return false;
-}
+})->add($mw);
